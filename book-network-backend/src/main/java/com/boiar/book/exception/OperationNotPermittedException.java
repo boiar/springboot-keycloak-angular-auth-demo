569f0b536287233938d1c06ec64ea45eb1143929
boiar <kero2000boiar410@gmail.com>
2026-01-22 13:34:39 +0200

    edits

---
+++ book-network-backend/src/main/java/com/boiar/book/exception/OperationNotPermittedException.java
@@ -0,0 +1,11 @@
+package com.boiar.book.exception;
+
+public class OperationNotPermittedException extends RuntimeException {
+
+    public OperationNotPermittedException() {
+    }
+
+    public OperationNotPermittedException(String message) {
+        super(message);
+    }
+}
---
+++ book-network-backend/src/test/java/com/alibou/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boiar.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
