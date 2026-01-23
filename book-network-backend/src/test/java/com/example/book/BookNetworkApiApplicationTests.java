ac9f22584b4e485ba5d2e78afb2dfb089ada7f42
boiar <kero2000boiar410@gmail.com>
2026-01-23 13:37:10 +0200

    edits

---
+++ book-network-backend/src/test/java/com/example/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
